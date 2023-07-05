cd2e44889c4594f1f44e139867de869ee0da893a
kaffieneaddikt <alex.s.pinkerton@outlook.com>
2023-07-04 20:33:16 -1000

    changed array lists to arrays, adjusted syntax

--- testscript.ps1
+++ testscript.ps1
@@ -6,26 +6,26 @@ $vhdPath = "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks"
 $vhdParentPath = "C:\Program Files\Microsoft Learning\Base\Base22A-WS22-2348.vhd"
 
 # Create list of desired computer names
-[System.Collections.ArrayList]$serverList = @("KEMP-RTR1","KEMP-DC1","DURANT-RTR1","DURANT-DC1","PAYTON-RTR1","PAYTON-DC1")
+$servers = "KEMP-RTR1","KEMP-DC1","DURANT-RTR1","DURANT-DC1","PAYTON-RTR1","PAYTON-DC1"
 
 # Create a list of desired switch names
-[System.Collections.ArrayList]$switchList = @("SeattleSwitch","CapitalSwitch","DallasSwitch","SEAtoDAL","SEAtoCAP","SEAtoINET")
+$switches = "SeattleSwitch","CapitalSwitch","DallasSwitch","SEAtoDAL","SEAtoCAP","SEAtoINET"
 
 # Use $serverList to create VHD names
-[System.Collections.ArrayList]$vhdList = forEach ($server in $serverList) {
-  $vhdList + ($server + ".vhd")
+$vhds = foreach ($server in $servers) {
+  $vhds + ($server + ".vhd")
 }
 
 # Create VHDs
-foreach ($vhd in $vhdList) {
+foreach ($vhd in $vhds) {
   $vhdPathInternal = "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\$vhd"
-  New-VHD -ParentPath $vhdParentPath -path $vhdPathInternal
+  New-VHD -ParentPath $vhdParentPath -path $vhdPathInternal -Differencing 
 }
 
 # Create VMs
-foreach ($vhd in $vhdList) {
+foreach ($server in $servers) {
   $VM = @{
-  Name = $vhd
+  Name = $server
   MemoryStartupBytes = 2147483648
   Generation = 1
   BootDevice = "VHD"
@@ -36,35 +36,34 @@ foreach ($vhd in $vhdList) {
 }
 
 # Create Virtual Switches
-foreach ($switch in $switchList) {
+foreach ($switch in $switches) {
   New-VMSwitch -Name $switch -SwitchType Private
 }
 
 
 # Add locational switches to both DC and RTR for each geographic location
-foreach ($vhd in $vhdList) {
-  if($vhd -like "KEMP*") {
-    Connect-VMNetworkAdapter -VMName $vhd -SwitchName SeattleSwitch
-  } elseif ($vhd -like "DURANT*") {
-    Connect-VMNetworkAdapter -VMName $vhd -SwitchName DallasSwitch
-  } elseif ($vhd -like "PAYTON*") {
-    Connect-VMNetworkAdapter -VMName $vhd -SwitchName CapitalSwitch
+foreach ($server in $servers) {
+  if($server -like "KEMP*") {
+    Get-VMNetworkAdapter -VMName $server | Connect-vmnetworkadapter -SwitchName SeattleSwitch
+  } elseif ($server -like "DURANT*") {
+    Get-VMNetworkAdapter -VMName $server | Connect-vmnetworkadapter -SwitchName DallasSwitch
+  } elseif ($server -like "PAYTON*") {
+    Get-VMNetworkAdapter -VMName $server | Connect-VMNetworkAdapter -SwitchName CapitalSwitch
   }
 }
 
 # Add the remaining switches to the appropriate routers
-foreach ($vhd in $vhdList) {
-  if($vhd -eq "KEMP-RTR1.vhd") {
-    [System.Collections.ArrayList]$seaSwitches = @("SEAtoINET","SEAtoDAL","SEAtoCAP")
+foreach ($server in $servers) {
+  if($server -eq "KEMP-RTR1") {
+    $seaSwitches = "SEAtoINET","SEAtoDAL","SEAtoCAP"
     foreach ($seaSwitch in $seaSwitches) {
-      Add-VMNetworkAdapter -VMName $vhd -SwitchName $seaSwitch
-      Connect-VMNetworkAdapter -VMName $vhd -SwitchName $seaSwitch
+      Add-VMNetworkAdapter -VMName $server -Name "$seaSwitch" -SwitchName $seaSwitch
     }
-  } elseif ($vhd -eq "DURANT-RTR1.vhd") {
-    Add-VMNetworkAdapter -VMName $vhd -Name SEAtoDAL -SwitchName SEAtoDAL
-    Connect-VMNetworkAdapter -VMName $vhd -SwitchName SEAtoDAL
-  } elseif ($vhd -eq "PAYTON-RTR1.vhd") {
-    Add-VMNetworkAdapter -VMName $vhd -SwitchName SEAtoCAP
-    Connect-VMNetworkAdapter -VMName $vhd -SwitchName SEAtoCAP
+  } elseif ($server -eq "DURANT-RTR1") {
+    Add-VMNetworkAdapter -VMname $server -Name SEAtoDAL -switchname SEAtoDAL
+  } elseif ($server -eq "PAYTON-RTR1") {
+    Add-VMNetworkAdapter -VMName $server -name SEAtoCAP -SwitchName SEAtoCAP
   } 
 }
+
+<# TESTED AND IT WORKS! #>
